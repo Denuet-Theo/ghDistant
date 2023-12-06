018edca32078ad8183eba7b4cfda729b658d1304
denue <denuettheo@gmail.com>
2023-12-06 16:01:30 +0100

    C'6 fond d'ecran vert

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,7 +6,7 @@ Mettre la dimension à 150, 200
 Configurer la fenetre sur ‘exitOnClose’
 // Création du label de texte
 Créer un label avec du texte centré et contenant le texte ‘HELLO_WORLD_TEXT’
-Créer la couleur jaune
+Créer la couleur vert
 Mettre la couleur bleu sur le label
 // Finalisation
 Ajouter le lable sur la fenetre
