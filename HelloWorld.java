b769a6b02285eac943d18d53cbeee7de94160988
Denuet-Theo <120714692+Denuet-Theo@users.noreply.github.com>
2023-12-10 18:21:09 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,8 @@ Algo HelloWorld {
  fonction principale {
 // Création de la fenetre
  Créer une fenetre de titre ‘HELLO_WORLD_TITLE’
-Mettre la dimension à 150, 200
+  // Fix #5 modif taille
+Mettre la dimension à 110, 200
 Configurer la fenetre sur ‘exitOnClose’
 // Création du label de texte
 Créer un label avec du texte centré et contenant le texte ‘HELLO_WORLD_TEXT’
