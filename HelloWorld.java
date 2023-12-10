6a5ba01fe1879fb1921b078c98149e110538c6f1
deudss <153448985+deudss@users.noreply.github.com>
2023-12-10 17:51:32 +0100

    fix #1 changement couleur

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,7 +7,8 @@ Mettre la dimension à 150, 200
 Configurer la fenetre sur ‘exitOnClose’
 // Création du label de texte
 Créer un label avec du texte centré et contenant le texte ‘HELLO_WORLD_TEXT’
-Créer la couleur vert
+ //fix #1 Il faudrait changer la couleur
+Créer la couleur cyan
 Mettre la couleur bleu sur le label
 // Finalisation
 Ajouter le lable sur la fenetre
